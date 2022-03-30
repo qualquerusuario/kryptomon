a04adcd00e99a8fd898a46038773822c6ebb440f
Miguel <miguelsilvavianaa@gmail.com>
2022-03-30 16:03:31 -0300

    Bot kryptomon missão 18hrs

--- main.py
+++ main.py
@@ -1,64 +1,78 @@
 import pyautogui
 import time
 
+kryptomon_x = 1089
+kryptomon_y = 660
+contador = 0
 pyautogui.sleep(1)
 
 
-def comida():
-    time.sleep(12)
-    pyautogui.click(x=813, y=780)
+def comida(x, y):
+    time.sleep(5)
+    pyautogui.click(x=810, y=881)
     time.sleep(1)
-    pyautogui.click(x=732, y=863)
+    pyautogui.click(x=737, y=965)
     pyautogui.mouseDown()
     pyautogui.mouseDown()
-    pyautogui.moveTo(x=963, y=628)
+    pyautogui.moveTo(x, y)
     time.sleep(1)
     pyautogui.mouseUp()
 
 
-def vida():
+def vida(x, y):
     time.sleep(6)
-    pyautogui.click(x=897, y=782)
+    pyautogui.click(x=899, y=879)
     time.sleep(2)
-    pyautogui.click(x=729, y=866)
+    pyautogui.click(x=737, y=965)
     time.sleep(2)
     pyautogui.mouseDown()
     pyautogui.mouseDown()
-    pyautogui.moveTo(x=963, y=628)
+    pyautogui.moveTo(x, y)
     time.sleep(1)
     pyautogui.mouseUp()
 
 
-def toys():
+def toys(x, y):
     time.sleep(6)
-    pyautogui.click(x=985, y=783)
+    pyautogui.click(x=984, y=883)
     time.sleep(2)
-    pyautogui.click(x=734, y=858)
+    pyautogui.click(x=737, y=965)
     time.sleep(2)
     pyautogui.mouseDown()
     pyautogui.mouseDown()
-    pyautogui.moveTo(x=963, y=628)
+    pyautogui.moveTo(x, y)
     time.sleep(1)
     pyautogui.mouseUp()
 
 
-def treinar():
+def treinar(x, y, treino):
     time.sleep(7)
-    pyautogui.click(x=1069, y=784)
+    pyautogui.click(x=1065, y=880)
     time.sleep(2)
-    pyautogui.click(x=1014, y=861)
+    pyautogui.click(treino, y=960)
     time.sleep(2)
     pyautogui.mouseDown()
     pyautogui.mouseDown()
-    pyautogui.moveTo(x=963, y=628)
+    pyautogui.moveTo(x, y)
     time.sleep(1)
     pyautogui.mouseUp()
 
 
 
 while True:
-    comida()
-    vida()
-    toys()
-    treinar()
+    contador += 1
+    comida(x = kryptomon_x ,y =kryptomon_y)
+    vida(x = kryptomon_x ,y =kryptomon_y)
+    toys(x = kryptomon_x ,y =kryptomon_y)
+    if contador == 1:
+        treinar(x=kryptomon_x, y=kryptomon_y, treino=739)
+    elif contador == 2:
+        treinar(x=kryptomon_x, y=kryptomon_y, treino=878)
+    elif contador == 3:
+        treinar(x=kryptomon_x, y=kryptomon_y, treino=1011)
+    else:
+        treinar(x=kryptomon_x, y=kryptomon_y, treino=1151)
+        time.sleep(2)
+        contador = 0
+
     time.sleep(1800)
